6c2de31bf960cd4a11ba685ffc8a65febb8426f7
van Dijk Daniel <scur0220@int6.local.snellius.surf.nl>
2024-04-09 17:19:16 +0200

    improved vocab

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,17 +1,8 @@
 import json
 import spacy
 import time
-from collections import Counter, OrderedDict, defaultdict
 from spacy.lang.en import English
-# # execute: "$python -m spacy download en" if en_core_web_sm can not be found.
-# # see https://stackoverflow.com/questions/54334304/spacy-cant-find-model-en-core-web-sm-on-windows-10-and-python-3-5-3-anacon
-
-# # only include tokenization for speed
-# nlp = spacy.load("en_core_web_sm", disable=["parser", "ner", "lemmatizer", "attribute_ruler"])
-
-
-nlp = English()
-tokenizer = nlp.tokenizer
+import numpy as np
 
 def read_json(split='dev'):
     data = []
@@ -25,6 +16,9 @@ def read_json(split='dev'):
 
 def preprocess(split='dev'):
     """lowering and tokenization of sentences"""
+
+    nlp = English()
+    tokenizer = nlp.tokenizer
     data = read_json(split)
     preprocessed = []
 
@@ -40,42 +34,48 @@ def preprocess(split='dev'):
     print(f" It took {(end - start):.2f} seconds to tokenize {split} split")
     return preprocessed
 
-# Below is taken from assignment 2 of NLP1 
-
-class OrderedCounter(Counter, OrderedDict):
-  """Counter that remembers the order elements are first seen"""
-  def __repr__(self):
-    return '%s(%r)' % (self.__class__.__name__,
-                      OrderedDict(self))
-  def __reduce__(self):
-    return self.__class__, (OrderedDict(self),)
-
-
 class Vocabulary:
-  """A vocabulary, assigns IDs to tokens"""
-
+  """
+  Create vocabulary by:
+  - generating a set of the words in corpus
+  - put in list with <unk> and <pad> and use index of this list as ID
+  - sort vocab set such that ID's will be the same across runs.
+  """
   def __init__(self):
-    self.freqs = OrderedCounter()
-    self.w2i = {}
-    self.i2w = []
+    self.vocab = set()
+    self.mapping = []
 
-  def count_token(self, t):
-    self.freqs[t] += 1
+  def add_token(self, token):
+    self.vocab.add(token)
+  
+  def create_mapping(self):
+    # set <unk> and <pad> at position 0 and 1 and append sorted vocab set as list.
+    vocab_list = ['<unk>', '<pad>'] + sorted(list(self.vocab))
+    # make mapping where key is word and value is index in index list
+    self.mapping = {index: word for word, index in enumerate(vocab_list)}
 
-  def add_token(self, t):
-    self.w2i[t] = len(self.w2i)
-    self.i2w.append(t)
 
-  def build(self, min_freq=0):
-    '''
-    min_freq: minimum number of occurrences for a word to be included
-              in the vocabulary
-    '''
-    self.add_token("<unk>")  # reserve 0 for <unk> (unknown words)
-    self.add_token("<pad>")  # reserve 1 for <pad> 
+# v = Vocabulary()
+# for pair in preprocessed:
+#   for token in pair['sentence_1'] + pair['sentence_2']:
+#     v.add_token(token.text)
+# v.create_mapping()
 
-    tok_freq = list(self.freqs.items())
-    tok_freq.sort(key=lambda x: x[1], reverse=True)
-    for tok, freq in tok_freq:
-      if freq >= min_freq:
-        self.add_token(tok)
+
+def get_glove():
+    with open("./SentEval/pretrained/glove.840B.300d.txt", 'r') as file:
+      glove = file.readlines()
+    v_glove = Vocabulary()
+    vectors = []
+    # <unk> and <pad> are zero-initialized
+    vectors.append([0]*300) # <unk>
+    vectors.append([0]*300) # <pad>
+    for line in glove[:10000]:
+        token = line.split()[0]
+        v_glove.add_token(token)
+        embedding = line.split()[1:]
+        vectors.append(embedding)
+    v_glove.create_mapping()
+    print("Vocabulary size:", len(v_glove.mapping))
+    vectors = np.stack(vectors, axis=0).astype(float)
+    return v_glove, vectors
--- src/train.py
+++ src/train.py
@@ -6,77 +6,32 @@ from preprocess import *
 import numpy as np
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
-
-
-def get_glove():
-    with open("/home/scur0220/assignment1/sentence-representations-with-NLI-from-scratch/src/SentEval/pretrained/glove.840B.300d.txt", 'r') as file:
-      glove = file.readlines()
-    v_glove = Vocabulary()
-    vectors = []
-    # <unk> and <pad> are zero-initialized
-    # embeddings for <unk> and <pad>,
-    vectors.append([0]*300)
-    vectors.append([0]*300)
-    for line in glove[:100]:
-        token = line.split()[0]
-        v_glove.count_token(token)
-        embedding = line.split()[1:]
-        vectors.append(embedding)
-    v_glove.build()
-    print("Vocabulary size:", len(v_glove.w2i))
-    vectors = np.stack(vectors, axis=0).astype(float)
-    return v_glove, vectors
-
-v_glove, vectors = get_glove()
-
-
-class BOW(nn.Module):
-  """Averaging pre-trained word embeddings"""
-
-  def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, vocab):
-    super(BOW, self).__init__()
-    self.vocab = vocab
-    self.embed = nn.Embedding(vocab_size, embedding_dim)
-    self.bias = nn.Parameter(torch.zeros(embedding_dim), requires_grad=True)
-    self.linear = nn.Linear(embedding_dim, output_dim)
-
-  def forward(self, inputs):
-    embeds = self.embed(inputs)
-    logits = embeds.sum(1)  + self.bias
-
-    logits = self.linear(logits)
-
-    return logits
-  
 seed = 42
 torch.manual_seed(seed)
 
-i2t = labels = ['neutral', 'entailment', 'contradiction']
-t2i = OrderedDict({p : i for p, i in zip(i2t, range(len(i2t)))})
-print(t2i)
-print(t2i['neutral'])
+print(device)
+
+v_glove, vectors = get_glove()
+
+labels = ['neutral', 'entailment', 'contradiction']
+label_mapping = {index: label for label, index in enumerate(labels)}
 
 
-bow = BOW(len(v_glove.w2i), 300, 100, len(t2i), vocab=v_glove)
 
-# copy pre-trained word vectors into embeddings table
-bow.embed.weight.data.copy_(torch.from_numpy(vectors))
-bow.embed.weight.requires_grad = False
-bow = bow.to(device)
-
-
-def prep(pair, vocab, t2i):
-  # vocab returns 0 if the word is not there (i2w[0] = <unk>)
-  x1 = [vocab.w2i.get(t, 0) for t in pair['sentence_1']]
+def prep(pair, vocab, label_mapping):
+  x1 = []
+  for token in pair['sentence_1']:
+    x1.append(vocab.mapping.get(token.text, 0))
   x1 = torch.LongTensor([x1])
   x1 = x1.to(device)
 
-  x2 = [vocab.w2i.get(t, 0) for t in pair['sentence_2']]
+  x2 = []
+  for token in pair['sentence_2']:
+    x2.append(vocab.mapping.get(token.text, 0))
   x2 = torch.LongTensor([x2])
   x2 = x2.to(device)
 
-  y = torch.LongTensor([t2i[pair['gold_label']]])
+  y = torch.LongTensor([label_mapping[pair['gold_label']]])
   y = y.to(device)
 
   return x1, x2, y
@@ -85,6 +40,10 @@ dev_split = preprocess(split='dev')
 
 print(dev_split[0])
 pair = dev_split[0]
-print(prep(pair, bow.vocab, t2i))
+x1, x2, y = prep(pair, v_glove, label_mapping)
+print(x1)
+print(x2)
+print(y)
+#print(bow(x1))
 # optimizer = optim.Adam(bow.parameters(), lr=0.0005)
 
