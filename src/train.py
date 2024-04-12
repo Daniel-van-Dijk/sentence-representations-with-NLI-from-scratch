a4a9412409e5e79038f1cc2334d49831f99edfeb
Daniël van Dijk <daniel@cargoprobe.com>
2024-04-12 17:51:51 +0200

    lstm in train

--- src/train.py
+++ src/train.py
@@ -6,6 +6,7 @@ from preprocess import *
 from torch.nn.utils.rnn import pad_sequence
 import numpy as np
 from models.bow import BOW
+from models.lstm import LSTM_NLI
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 seed = 42
 torch.manual_seed(seed)
@@ -19,31 +20,41 @@ labels = ['neutral', 'entailment', 'contradiction']
 # map label to numeric
 label_mapping = {label: index for index, label in enumerate(labels)}
 
-train_split = preprocess(split='dev')
+train_split = preprocess(split='train')
 vocab = create_vocab(train_split)
-embeddings = align_vocab_with_glove(vocab)
+#embeddings = align_vocab_with_glove(vocab)
+embeddings = np.random.rand(len(vocab.mapping), 300)
+#print(embeddings.shape)
 
 # TextpairDataset and custom_collate function in preprocess.py
 train_data = TextpairDataset(train_split, vocab, label_mapping)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True, collate_fn=custom_collate)
 
-validation_split = preprocess(split='test')
+validation_split = preprocess(split='dev')
 validation_data = TextpairDataset(validation_split, vocab, label_mapping)
 validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=64, shuffle=False, collate_fn=custom_collate)
 
 
 embedding_dim = 300
 vocab_size = len(vocab.mapping)
-print(vocab_size)
+print(f'size of vocab: {vocab_size}')
 hidden_dim = 512
-bow = BOW(embedding_dim, hidden_dim, vocab_size, len(labels) )
-bow.token_embeddings.weight.data.copy_(torch.from_numpy(embeddings))
-bow.token_embeddings.weight.requires_grad = False
-bow.to(device)
+# bow = BOW(embedding_dim, hidden_dim, vocab_size, len(labels) )
+# bow.token_embeddings.weight.data.copy_(torch.from_numpy(embeddings))
+# bow.token_embeddings.weight.requires_grad = False
+# bow.to(device)
+
+# lstm dim from tabel 3 in paper
+lstm_dim = 2048
+lstm = LSTM_NLI(embedding_dim, lstm_dim, hidden_dim, vocab_size, len(labels))
+lstm.token_embeddings.weight.data.copy_(torch.from_numpy(embeddings))
+lstm.token_embeddings.weight.requires_grad = False
+lstm.to(device)
+
 
 loss_module = nn.CrossEntropyLoss()
 
-def evaluate(model, dataloader, loss_module):
+def evaluate(model, dataloader, loss_module, model_flag):
     model.eval()
     val_loss = 0
     total_preds, correct_preds = 0, 0
@@ -52,7 +63,11 @@ def evaluate(model, dataloader, loss_module):
             sent1s = sent1s.to(device)
             sent2s = sent2s.to(device)
             labels = labels.to(device)
-            output = model(sent1s, sent2s)
+            if model_flag == 'bow':
+                output = model(sent1s,sent2s)
+
+            elif model_flag == 'lstm':
+                output = model(sent1s,lengths1, sent2s, lengths2)
             predicted_labels = torch.argmax(output, dim=1)
             correct_preds += (predicted_labels == labels).sum() 
             total_preds += labels.shape[0]
@@ -61,7 +76,7 @@ def evaluate(model, dataloader, loss_module):
     return val_loss / len(dataloader), correct_preds / total_preds
     
 
-def train(train_loader, validation_loader, model, loss_module, num_epochs=5):
+def train(train_loader, validation_loader, model, loss_module, num_epochs=50, model_flag='bow'):
     optimizer = optim.SGD(model.parameters(), lr=0.1)
     # learning rate * 0.99 after every epoch
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.99)
@@ -69,14 +84,17 @@ def train(train_loader, validation_loader, model, loss_module, num_epochs=5):
     for epoch in range(num_epochs):
         model.train()
         train_loss = 0
-        print(f'starting epoch {epoch} with learning rate: {scheduler.get_last_lr()}')
+        print(f'starting epoch {epoch} with learning rate: {scheduler.get_last_lr()[0]}')
         for sent1s, sent2s, labels, lengths1, lengths2 in train_loader:
             sent1s = sent1s.to(device)
             sent2s = sent2s.to(device)
             labels = labels.to(device)
             optimizer.zero_grad()
-            output = model(sent1s, sent2s)
+            if model_flag == 'bow':
+                output = model(sent1s,sent2s)
 
+            elif model_flag == 'lstm':
+                output = model(sent1s,lengths1, sent2s, lengths2)
             # print('softmax output', output)
             # print('softmax output shape', output.shape)
             # print('labels', labels)
@@ -85,7 +103,7 @@ def train(train_loader, validation_loader, model, loss_module, num_epochs=5):
             train_loss += output_loss.item()
             output_loss.backward()
             optimizer.step()
-        validation_loss, validation_accuracy = evaluate(model, validation_loader, loss_module)
+        validation_loss, validation_accuracy = evaluate(model, validation_loader, loss_module, model_flag)
         
         print(f'avg training loss at epoch {epoch}: {validation_loss}')
         print(f'Validation accuracy at epoch {epoch}: {validation_accuracy }%')
@@ -96,6 +114,7 @@ def train(train_loader, validation_loader, model, loss_module, num_epochs=5):
                 group['lr'] /= 5
         last_validation_accuracy = validation_accuracy
         scheduler.step()
-        if scheduler.get_last_lr() < 0.00001:
+        if scheduler.get_last_lr()[0] < 0.00001:
+            print('reached treshold, stopping training')
             break
-train(train_loader, validation_loader, bow, loss_module)
+train(train_loader, validation_loader, lstm, loss_module, model_flag='lstm')
